1e37126eb91f7ce60fcea87b9c8735e9a87618ad
brandenreilly <155998782+brandenreilly@users.noreply.github.com>
2024-02-02 20:24:25 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+  console.log();
+};
+
+let generateExcuse = () => {
+  let who = ["my dog", "My grandma", "my turtle", "My bird"];
+  let action = ["ate", "took", "left", "crushed", "broke"];
+  let item = ["my homework", "my bicycle", "my car", "my shoe", "my keys"];
+  let where = [
+    "on the street",
+    "in my house",
+    "in the parking garage",
+    "before class"
+  ];
+
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let actIndx = Math.floor(Math.random() * action.length);
+  let itemIndx = Math.floor(Math.random() * item.length);
+  let whereIndx = Math.floor(Math.random() * where.length);
+
+  return (
+    who[whoIndx] +
+    " " +
+    action[actIndx] +
+    " " +
+    item[itemIndx] +
+    " " +
+    where[whereIndx]
+  );
 };
